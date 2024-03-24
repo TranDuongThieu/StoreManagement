f3da2c0a7b63e631ce1f2718fe41fc115d0991d8
tranduongthieu <thieu098dm3@gmail.com>
2024-03-24 11:16:44 +0700

    Initial Project

---
+++ src/main/java/com/hcmute/storemanagement/StoreManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hcmute.storemanagement;
+
+/**
+ *
+ * @author thieu
+ */
+public class StoreManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
